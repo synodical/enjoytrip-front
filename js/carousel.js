590a7cb2d150944e145915791f76d3ec119e8fdf
synodical <lena0323park@naver.com>
2023-03-17 22:27:25 +0900

    auth

--- js/carousel.js
+++ js/carousel.js
@@ -1,21 +1,21 @@
-axios.defaults.baseURL = "http://apis.data.go.kr/B551011/KorService1/";
+axios.defaults.baseURL = 'http://apis.data.go.kr/B551011/KorService1/';
 
 let json;
 
 async function getDetailCommon(contentId) {
   try {
-    const res = await axios.get("/detailCommon1", {
+    const res = await axios.get('/detailCommon1', {
       params: {
-        _type: "json",
-        MobileOS: "WIN",
-        MobileApp: "runinto",
+        _type: 'json',
+        MobileOS: 'WIN',
+        MobileApp: 'runinto',
         contentId: contentId,
-        defaultYN: "Y",
-        firstImageYN: "Y",
-        addrinfoYN: "Y",
+        defaultYN: 'Y',
+        firstImageYN: 'Y',
+        addrinfoYN: 'Y',
         ServiceKey:
           // process.env.API_SERVICE_KEY,
-          "zwSMywdlgo3E03aEm4hFNEzkpaACYU80Fp/lGBjG7Q1b4TGfAbc+/w85V/Ge6H9XPBzoZecK2Jf4fEfuwd0zdA==",
+          'zwSMywdlgo3E03aEm4hFNEzkpaACYU80Fp/lGBjG7Q1b4TGfAbc+/w85V/Ge6H9XPBzoZecK2Jf4fEfuwd0zdA==',
       },
     });
     json = res.data.response.body.items.item;
@@ -25,14 +25,14 @@ async function getDetailCommon(contentId) {
 }
 
 async function main() {
-  let contentId = 126528;
+  let contentId = 126532;
 
-  for (let i = 1; i <= 3; i++) {
+  for (let i = 1; i <= 4; i++) {
     const curId = contentId + i - 1;
 
     await getDetailCommon(curId);
     let img = document.getElementById(`carousel-img${i}`);
-    img.setAttribute("src", json[0].firstimage);
+    img.setAttribute('src', json[0].firstimage);
     document.getElementById(`section1-addr${i}`).innerText = json[0].addr1;
   }
 }
--- js/mypage.js
+++ js/mypage.js
@@ -1,11 +1,14 @@
 function mypageSet() {
-    let navId = document.getElementById('navId').value
-    let navName = document.getElementById('navName').value
-    let navPwd = document.getElementById('navPwd').value
-    let navPwCheck = document.getElementById('navPwCheck').value
-    
-    localStorage.setItem(1, navName);
-    localStorage.setItem(2, navId);
-    localStorage.setItem(3, navPwd);
-    localStorage.setItem(4, navPwCheck);
+  let navId = document.getElementById('navId').value;
+  let navName = document.getElementById('navName').value;
+  let navPwd = document.getElementById('navPwd').value;
+  let navPwCheck = document.getElementById('navPwCheck').value;
+
+  localStorage.setItem(1, navName);
+  localStorage.setItem(2, navId);
+  localStorage.setItem(3, navPwd);
+  localStorage.setItem(4, navPwCheck);
+}
+function logout() {
+  localStorage.clear();
 }
