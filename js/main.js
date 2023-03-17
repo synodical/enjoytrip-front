eaedd6cb76fda8a0ea3a59d2f3417b7ad1c62518
이예은 <yeeun_1ee@naver.com>
2023-03-17 15:04:42 +0900

    궁 정보 가져오기

--- js/main.js
+++ js/main.js
@@ -1,18 +1,40 @@
 // const axios = require("axios");
-axios.defaults.baseURL = 'http://apis.data.go.kr/B551011/KorService1/';
+axios.defaults.baseURL = "http://apis.data.go.kr/B551011/KorService1/";
 
 var json;
 // 지역기반 관광정보 조회
 async function getAreaList() {
   try {
-    const res = await axios.get('/areaBasedList1', {
+    const res = await axios.get("/areaBasedList1", {
       params: {
-        _type: 'json',
-        MobileOS: 'WIN',
-        MobileApp: 'hi',
+        _type: "json",
+        MobileOS: "WIN",
+        MobileApp: "runinto",
         ServiceKey:
           // process.env.API_SERVICE_KEY,
-          'zwSMywdlgo3E03aEm4hFNEzkpaACYU80Fp/lGBjG7Q1b4TGfAbc+/w85V/Ge6H9XPBzoZecK2Jf4fEfuwd0zdA==',
+          "zwSMywdlgo3E03aEm4hFNEzkpaACYU80Fp/lGBjG7Q1b4TGfAbc+/w85V/Ge6H9XPBzoZecK2Jf4fEfuwd0zdA==",
+      },
+    });
+    json = res.data.response.body.items.item;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function getDetailCommon(contentId) {
+  try {
+    const res = await axios.get("/detailCommon1", {
+      params: {
+        _type: "json",
+        MobileOS: "WIN",
+        MobileApp: "runinto",
+        contentId: contentId,
+        defaultYN: "Y",
+        firstImageYN: "Y",
+        addrinfoYN: "Y",
+        ServiceKey:
+          // process.env.API_SERVICE_KEY,
+          "zwSMywdlgo3E03aEm4hFNEzkpaACYU80Fp/lGBjG7Q1b4TGfAbc+/w85V/Ge6H9XPBzoZecK2Jf4fEfuwd0zdA==",
       },
     });
     json = res.data.response.body.items.item;
@@ -22,10 +44,18 @@ async function getAreaList() {
 }
 
 async function main() {
-  await getAreaList();
-  console.log(json);
-  for (let i of json) {
-    console.log(i);
+  let contentId = 126508;
+
+  for (let i = 1; i <= 4; i++) {
+    const curId = contentId + i - 1;
+
+    await getDetailCommon(curId);
+    console.log(json);
+    let img = document.getElementById(`section1-img${i}`);
+    let title = document.getElementById(`section1-title${i}`);
+    img.setAttribute("src", json[0].firstimage);
+    title.innerText = json[0].title;
+    document.getElementById(`section1-addr${i}`).innerText = json[0].addr1;
   }
 }
 main();
