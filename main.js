db604ebf45da49d82b9f2430c5da5cc708df2311
이예은 <yeeun_1ee@naver.com>
2023-03-17 10:53:00 +0900

    fix mixed content err

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 // const axios = require("axios");
-axios.defaults.baseURL = "http://apis.data.go.kr/B551011/KorService1/";
+axios.defaults.baseURL = 'http://apis.data.go.kr/B551011/KorService1/';
 
 var json;
 // 지역기반 관광정보 조회
 async function getAreaList() {
   try {
-    const res = await axios.get("/areaBasedList1", {
+    const res = await axios.get('/areaBasedList1', {
       params: {
-        _type: "json",
-        MobileOS: "WIN",
-        MobileApp: "hi",
+        _type: 'json',
+        MobileOS: 'WIN',
+        MobileApp: 'hi',
         ServiceKey:
           // process.env.API_SERVICE_KEY,
-          "zwSMywdlgo3E03aEm4hFNEzkpaACYU80Fp/lGBjG7Q1b4TGfAbc+/w85V/Ge6H9XPBzoZecK2Jf4fEfuwd0zdA==",
+          'zwSMywdlgo3E03aEm4hFNEzkpaACYU80Fp/lGBjG7Q1b4TGfAbc+/w85V/Ge6H9XPBzoZecK2Jf4fEfuwd0zdA==',
       },
     });
     json = res.data.response.body.items.item;
